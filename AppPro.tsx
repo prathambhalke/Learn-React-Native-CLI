ed3543693d7ad1d2c00d9a31fc70e918f9e865ae
prathambhalke <prathameshbhalke125@gmail.com>
2024-11-27 01:01:39 +0530

    Theme Toggle  useColorScheme

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Switch, useColorScheme} from 'react-native';
 
 function AppPro(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [theme, setTheme] = useState(isDarkMode);
+
+  const containerStyle = [
+    Styles.container,
+    {backgroundColor: theme ? '#000' : '#fff'},
+  ];
+
+  const textStyle = theme ? Styles.whiteText : Styles.blackText;
+  const borderColor = theme ? 'green' : 'red';
+
   return (
-    <View style={Styles.container}>
-      <Text style={isDarkMode ? Styles.whiteText: Styles.blackText}>Hello World!</Text>
+    <View style={containerStyle}>
+      <View style={[Styles.innerContainer, {borderColor}]}>
+        <Text style={[textStyle, Styles.largeStyle]}>Toggle Theme</Text>
+        <Switch
+          value={theme}
+          onValueChange={() => {
+            setTheme(!theme);
+          }}
+        />
+      </View>
+      <Text style={textStyle}>Change the theme by toggling!</Text>
     </View>
   );
 }
@@ -13,13 +32,28 @@ function AppPro(): JSX.Element {
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderRadius: 50,
+    borderWidth: 2,
+    width: 300,
+    height: 100,
+    marginBottom: 10,
   },
   whiteText: {
     color: '#fff',
   },
   blackText: {
-    color: '#ffc0cb',
+    color: '#000',
+  },
+  largeStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
